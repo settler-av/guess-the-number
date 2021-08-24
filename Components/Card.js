bd3eb0493985abcfa16197c622b9b9e387080a7a
settler <adnanvahora144@gmail.com>
2021-08-24 16:35:16 +0530

    resolve the card issue

--- Components/Card.js
+++ Components/Card.js
@@ -1,23 +1,47 @@
+// import React from 'react';
+// import { View, StyleSheet } from 'react-native';
+
+// const Card = props => {
+//     return (
+//     <View style={{ ...styles.card, ...props.style }}> {props.children}</View>
+//     );
+// };
+
+// const styles = StyleSheet.create({
+//     card: {
+//         shadowColor: 'black',
+//         shadowOffset: { width: 0, height: 2 },
+//         shadowRadius: 6,
+//         shadowOpacity: 0.26,
+//         backgroundColor: 'white',
+//         elevation: 8,
+//         padding: 20,
+//         borderRadius: 11
+//     }
+// });
+
+// export default Card;
+
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 
 const Card = props => {
-    return (
-    <View style={{ ...styles.card, ...props.style }}> {props.children}</View>
-    );
+  return (
+    <View style={{ ...styles.card, ...props.style }}>{props.children}</View>
+  );
 };
 
 const styles = StyleSheet.create({
-    card: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: 2 },
-        shadowRadius: 6,
-        shadowOpacity: 0.26,
-        backgroundColor: 'white',
-        elevation: 10,
-        padding: 15,
-        borderRadius: 11
-    }
+  card: {
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+    elevation: 8,
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10
+  }
 });
 
 export default Card;
