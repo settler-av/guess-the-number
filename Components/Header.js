f12c58d90a47b364efad93d25220cff6a09631e8
settler <adnanvahora144@gmail.com>
2021-08-24 10:46:38 +0530

    Added original header

--- Components/Header.js
+++ Components/Header.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+
 const Header = props => {
-    <View styles = {styles.header}>
-        <Text style = {styles.headerTitle}>
-            {props.title}
-        </Text>
+  return (
+    <View style={styles.header}>
+      <Text style={styles.headerTitle}>{props.title}</Text>
     </View>
+  );
 };
+
 const styles = StyleSheet.create({
-    header: {
-        width: '100%',
-        height: 90,
-        paddingTop: 36,
-        backgroundColor : '#f7287b',
-        alignItems: 'center',
-        justifyContent : 'center'
-    },
-    headerTitle: {
-        color : 'black',
-        fontSize : 18
-    }
+  header: {
+    width: '100%',
+    height: 90,
+    paddingTop: 36,
+    backgroundColor: '#f7287b',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  headerTitle: {
+    color: 'black',
+    fontSize: 18
+  }
 });
+
 export default Header;
